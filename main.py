778939bb39085e3abe881ee0bc238638df8dcc6d
Romaindcrl <theflute07@gmail.com>
2026-09-08 12:25:07 +0200

    Refactor article handling in main.py by introducing Pydantic models for Article and ArticleInfo. Update list_articles and get_article functions to return structured data. Modify create_article to handle article creation with markdown conversion.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import html
-import json
 from pathlib import Path
 
 from fastapi import FastAPI, HTTPException
-from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
-
+from pydantic import BaseModel
 import markdown2
-from starlette.responses import Content
+
+class Article(BaseModel):
+    name: str
+    content: str
+    articleUrl: str
+    source: str
+
+class ArticleInfo(BaseModel):
+    name: str
+    articleUrl: str
 
 app = FastAPI()
 
@@ -23,45 +30,47 @@ wiki_dir: Path = Path(__file__).resolve().parent.parent / "wiki"
 
 
 @app.get("/list")
-def list_articles() -> list[dict[str, str]]:
-    articles = []
+def list_articles() -> list[ArticleInfo]:
+    articles: list[ArticleInfo] = []
     for article in (wiki_dir / "articles").glob("*.md"):
-        articles.append({
-            "name": article.stem.replace("_", " "),
-            "articleUrl": article.stem
-        })
+        articles.append(ArticleInfo(
+            name=article.stem.replace("_", " "),
+            articleUrl=article.stem
+        ))
 
     return articles
 
 @app.get("/article/{article_url}")
-def get_article(article_url: str) -> dict[str, str]:
+def get_article(article_url: str) -> Article:
     article_path: Path = Path(f"{wiki_dir}/articles/{article_url}.md")
     if article_path.is_file():
         with open(article_path, encoding="utf-8") as article_file:
             article_content: str = article_file.read()
             article_content_md: html = markdown2.markdown(article_content, extras=["latex"])
-        return {
-            "name": article_path.name,
-            "articleUrl": article_url,
-            "content": article_content_md,
-            "source": article_content,
-        }
+        return Article(
+            name=article_path.name,
+            articleUrl=article_url,
+            content=article_content_md,
+            source=article_content,
+        )
     raise HTTPException(status_code=404, detail="Article non trouvé")
 
 @app.post("/create")
-def create_article(body: dict) -> dict:
-    articleName: str = body["name"]
-    articleContent: str = body["content"]
-    articleUrl: str = articleName.replace(" ", "_")
-    article_path = wiki_dir / "articles" / f"{articleUrl}.md"
+def create_article(body: dict) -> Article:
+    article: Article = Article(
+        name=body["name"],
+        source=body["content"],
+        articleUrl=body["name"].replace(" ", "_"),
+        content=markdown2.markdown(body["content"], extras=["latex"]
+    ))
+    article_path = wiki_dir / "articles" / f"{article.articleUrl}.md"
     article_path.parent.mkdir(parents=True, exist_ok=True)
-    articleHTMLContent: html = markdown2.markdown(articleContent, extras=["latex"])
     
 
     with open(article_path, "w", encoding="utf-8") as fichier:
-        fichier.write(articleContent)
+        fichier.write(article.content)
 
-    article: dict = {"name": articleName, "articleUrl": articleUrl,  "content": articleHTMLContent, "source": articleContent}
+    article: dict = {"name": article.name, "articleUrl": article.articleUrl,  "content": article.content, "source": article.source}
     return article
 
 
